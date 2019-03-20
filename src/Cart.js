22b314ebe8ad6c30593e45454cdea8c658ddf45e
SwathiPrakaha <swathi.rao.pejakala@gmail.com>
2019-03-20 17:04:01 -0400

    Node starting with javascript

--- src/Cart.js
+++ src/Cart.js
@@ -1,3 +1,21 @@
+const Item = require('../src/Item.js')
+
 module.exports = class Cart {
 
+  constructor(totalPrice,items){
+      this.totalPrice = 0;
+      this.items=[];
+      this.myMap = new Map();
+  }
+
+  addItem(item,quantity){
+      this.myMap.set(item,quantity);
+
+  }
+ itemQuantities(){
+ return this.myMap.forEach(function (value, key, map) {
+    return (`m[${key.name}] = ${value}`);
+  });
+ }
 }
+
--- test/Cart.test.js
+++ test/Cart.test.js
@@ -1,11 +1,28 @@
 const Cart = require('../src/Cart.js');
 const expect = require('chai').expect;
+const Item = require('../src/Item.js')
 
 describe('Cart', () => {
     it('should initialize as empty', () => {
-        const cart = new Cart()
-
-        expect(cart.items).to.deep.equal([])
-        expect(cart.totalPrice).to.be.equal(0)
+       var items = [new Item("hb",50,true),new Item("wt",500,true)];
+        const cart = new Cart(10,items)
+    cart.addItem(items[0],52);
+    cart.addItem(items[1],2);
+    var itemKeys = cart.myMap.keys
+    
+      //expect(cart.items.length).to.be.equal(2);
+     // expect(cart.myMap.size).to.be.equal(2);
+     cart.myMap.forEach(function(value, key) {
+        console.log(key + ' = ' + value);
+      });
+     
+      function logMapElements(value, key, map) {
+        console.log(`m[${key.name}] = ${value}`);
+      }
+      cart.myMap.forEach(logMapElements);
+      expect(cart.itemQuantities.length).to.be.equal(2);
+     // expect(cart.myMap.get(`${key.name}`)).to.be.equal("hb");
+     //expect(cart.myMap.get(items[0])).to.be.equal(52);
+      //expect(cart.totalPrice).to.be.equal(10);
     })
 })
